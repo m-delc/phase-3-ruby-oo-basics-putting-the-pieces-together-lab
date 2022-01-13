2987e3333eba04d6bb3d783d64d8964c4a7e358b
Michael Delcambre <delcambrem@gmail.com>
2022-01-12 19:51:53 -0600

    lab complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+require 'pry'
 
+class Book
+    attr_accessor :title, :author
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :turn_page
+
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
